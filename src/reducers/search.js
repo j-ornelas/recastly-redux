f13b63c040fbeed1da07a927bb84843d2e4e4808
j-ornelas <johnnyoh1988@gmail.com>
2018-04-02 18:58:47 -0700

    Same as above

---
+++ src/reducers/search.js
@@ -0,0 +1,7 @@
+import Redux from 'redux';
+
+var searchReducer = (state = sampleData, action) => {
+  //TODO: define a reducer for the search field of our state.
+};
+
+export default searchReducer;
